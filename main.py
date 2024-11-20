9df0654e9d371408d53aa6e620be7248f77c9394
Abdulaziz Zayniev <zaynievlaziz@gmail.com>
2024-11-21 01:25:30 +0300

    some changes in classes

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Name:
     def __init__(self, surname: str, name: str):
         self._surname = surname
@@ -12,45 +15,61 @@ class Address:
 
 
 class Ticket:
-    def __init__(self, cost: int, departure_address: Address,
-                 destination_address: Address):
+    def __init__(self, cost: int, payment_time: str):
         self._cost = cost
-        self._departure_address = departure_address
-        self._destination_address = destination_address
+        self._payment_time = payment_time
+
+
+class Person:
+    def __init__(self, name: Name, age: int):
+        self._name = name
+        self._age = age
+
+
+class Passenger(Person):
+    def __init__(self, ticket: Ticket, name: Name, age: int):
+        super().__init__(name, age)
+        self._ticket = ticket
 
 
 class Transport:
     def __init__(self, license_plate: str,
-                 passenger_capacity: int, driver: Name):
+                 passenger_capacity: int, driver: Person, passengers: List[Passenger]):
         self._license_plate = license_plate
         self._passenger_capacity = passenger_capacity
         self._driver = driver
-        self.passengers = []
+        self._passengers = passengers
 
 
-class Passenger:
-    def __init__(self, name: Name, age: int, ticket: Ticket):
+class TransportationCompany:
+    def __init__(self, name: str, address: str, contact_info: str):
+        self.name = name
+        self.address = address
+        self.contact_info = contact_info
+
+class BusStop:
+    def __init__(self, name: str, location: str):
         self._name = name
-        self._age = age
-        self._ticket = ticket
+        self._location = location
+        self._routes = []
 
 
 class Bus(Transport):
-    def __init__(self, route_number: str, bus_stops: list, license_plate: str,
-                 passenger_capacity: int, driver: Name):
-        super().__init__(license_plate, passenger_capacity, driver)
+    def __init__(self, route_number: str, bus_stops: List[BusStop], transportation_company: TransportationCompany, license_plate: str,
+                 passenger_capacity: int, driver: Person, passengers: List[Passenger]):
+        super().__init__(license_plate, passenger_capacity, driver, passengers)
         self._route_number = route_number
         self._bus_stops = bus_stops
+        self._transportation_company = transportation_company
 
 
 class Taxi(Transport):
     def __init__(self, departure_address: Address,
-                 destination_address: Address, taxi_type: str,
+                 destination_address: Address,
                  travel_time: int, cost: int, license_plate: str,
-                 passenger_capacity: int, driver: Name):
-        super().__init__(license_plate, passenger_capacity, driver)
+                 passenger_capacity: int, driver: Person, passengers: List[Passenger]):
+        super().__init__(license_plate, passenger_capacity, driver, passengers)
         self._departure_address = departure_address
         self._destination_address = destination_address
-        self._taxi_type = taxi_type
         self._travel_time = travel_time
         self._cost = cost
